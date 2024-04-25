2c413b52e6999b1fda9cc1b20f6be2dd75d8599c
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-04-25 19:54:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here!
-
+let a=document.getElementById("infi-list");
+let i=1;
+for ( i = 1; i <= 10; i++) {
+	a.innerHTML+=`<li>Item ${i}</li>`;
+}
+a.addEventListener("scroll",fun);
+function fun() {
+	a.innerHTML+=`<li>Item ${i}</li>`;
+	a.innerHTML+=`<li>Item ${i+1}</li>`;
+	i=i+2;
+}
